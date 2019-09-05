5cabd569d8daeab0bdd27cdf68dbced945caa348
Patrick Harrison <jahman@bellatlantic.net>
2019-09-05 16:46:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
     students = []
-     binding.pry
+     
     
     page.css("div.student-card").each do |student|
       name = student.css(".student-name").text
@@ -19,6 +19,7 @@ class Scraper
     
       end
     students
+    binding.pry
   end
 
 
